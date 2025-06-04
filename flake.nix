1da323a4e12f015b1589f16f89148dbeb232f905
sidharthify <wednisegit@gmail.com>
2025-06-04 10:26:58 +0530

    local-update: 2025-06-04 10:26:58

--- flake.nix
+++ flake.nix
@@ -5,16 +5,25 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     playit-nixos-module.url = "github:pedorich-n/playit-nixos-module";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    zen-browser-source.url = "github:youwen5/zen-browser-flake";
   };
 
-  outputs = { self, nixpkgs, playit-nixos-module, spicetify-nix }: {
+  outputs = { self, nixpkgs, playit-nixos-module, spicetify-nix, zen-browser-source }: {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit spicetify-nix; };
+      specialArgs = {
+        inherit spicetify-nix;
+      };
       system = "x86_64-linux";
       modules = [
         playit-nixos-module.nixosModules.default
         spicetify-nix.nixosModules.spicetify
         ./configuration.nix
+
+        ({ pkgs, ... }: {
+          environment.systemPackages = with pkgs; [
+            zen-browser-source.packages.x86_64-linux.default
+          ];
+        })
       ];
     };
 
