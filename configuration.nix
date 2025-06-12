d54c7897e1e64fd85739b4061ac987b6ced510b6
sidharthify <wednisegit@gmail.com>
2025-06-12 16:26:50 +0530

    local-update: 2025-06-12 16:26:50

--- configuration.nix
+++ configuration.nix
@@ -449,8 +449,18 @@
   };
 
   # Cloudflared
-  services.cloudflared = {
+# Cloudflared
+services.cloudflared = {
   enable = true;
+  
+  tunnels.multi = {
+    credentialsFile = "/var/lib/cloudflared/multi.json";
+    default = "http_status:404";
+    ingress = {
+      "music.sidharthify.me" = "http://localhost:4533";
+      "nicotine.sidharthify.me" = "http://localhost:6080";
+    };
   };
+};
 
 }
