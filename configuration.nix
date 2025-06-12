cc744f201a610c56eac9831c4f1676ada123e0eb
sidharthify <wednisegit@gmail.com>
2025-06-12 18:12:03 +0530

    local-update: 2025-06-12 18:12:03

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,11 @@
   networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
   networking.interfaces.wlp3s0.useDHCP = false;
+  networking.interfaces."wlp3s0".ipv4.addresses = [{
+  address = "192.168.1.184";
+  prefixLength = 24;
+  }];
+  networking.defaultGateway = "192.168.1.1";
   networking.resolvconf.enable = false;
   networking.nameservers = [ "192.168.1.1" ];
   networking.interfaces."wlp3s0".mtu = 1500;
