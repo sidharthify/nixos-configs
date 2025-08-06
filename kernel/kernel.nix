5d3afeaefaa431e75f7484af27234869f0e67397
sidharthify <wednisegit@gmail.com>
2025-08-06 19:01:39 +0530

    local-update: 2025-08-06 19:01:39

--- kernel/kernel.nix
+++ kernel/kernel.nix
@@ -2,5 +2,5 @@
 
 { 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ "nvidia-modeset.hdmi_deepcolor=0" "nvidia.NVreg_EnableGpuFirmware=0" ];
+  boot.kernelParams = [ "nvidia-modeset.hdmi_deepcolor=0" "nvidia.NVreg_EnableGpuFirmware=0" "nvidia_drm.fbdev=1" "nvidia_drm.modeset=1"];
 }
