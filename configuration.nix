9e92213d99d5eb2a846da472596e374b4dc4d0fa
sidharthify <wednisegit@gmail.com>
2025-08-07 19:54:02 +0530

    local-update: 2025-08-07 19:54:02

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
     enable = true;
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
-    desktopManager.cinnamon.enable = true;
+    desktopManager.cinnamon.enable = false;
     desktopManager.gnome.enable = false;
   };
 
