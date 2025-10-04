dd7caac0c0fca2c003dae49162d681f24ac2075b
sidharthify <wednisegit@gmail.com>
2025-10-05 01:27:07 +0530

    local-update: 2025-10-05 01:27:07

--- configuration.nix
+++ configuration.nix
@@ -149,4 +149,13 @@
    nixpkgs.config.permittedInsecurePackages = [
      "electron-35.7.5"
    ];
+
+  # nh
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/etc/nixos"; # NH_OS_FLAKE variable
+  };
+
 }
