4310bdb016d2eeeac270e09e4fbee6fc79e7c42d
sidharthify <wednisegit@gmail.com>
2025-10-16 18:35:47 +0530

    local-update: 2025-10-16 18:35:47

--- flake.nix
+++ flake.nix
@@ -8,35 +8,45 @@
     zen-browser-source.url = "github:youwen5/zen-browser-flake";
     nixcord.url = "github:KaylorBen/nixcord";
     lazyvim-nix.url = "github:jla2000/lazyvim-nix";
-
+    nixstall.url = "github:sidharthify/nixstall";
     winboat.url = "github:TibixDev/winboat";
-
   };
 
-  outputs = { self, nixpkgs, home-manager, spicetify-nix, zen-browser-source, nixcord, lazyvim-nix, winboat, ... }: {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, spicetify-nix, zen-browser-source, nixcord, lazyvim-nix, nixstall, winboat, ... }:
+    let
       system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.sidharthify = import ./home-manager/home.nix;
-          home-manager.sharedModules = [
-            nixcord.homeModules.nixcord
-          ];
-          home-manager.extraSpecialArgs = {
-            inherit spicetify-nix lazyvim-nix;
-          };
-        }
-        ({ pkgs, ... }: {
-          environment.systemPackages = with pkgs; [
-            zen-browser-source.packages.x86_64-linux.default
-            winboat.packages.x86_64-linux.winboat
-          ];
-        })
-      ];
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.sidharthify = import ./home-manager/home.nix;
+
+            home-manager.sharedModules = [
+              nixcord.homeModules.nixcord
+            ];
+
+            home-manager.extraSpecialArgs = {
+              inherit spicetify-nix lazyvim-nix;
+            };
+          }
+
+          ({ pkgs, ... }: {
+            environment.systemPackages = with pkgs; [
+              zen-browser-source.packages.${system}.default
+              winboat.packages.${system}.winboat
+              nixstall.packages.${system}.default
+            ];
+          })
+        ];
+      };
     };
-  };
 }
